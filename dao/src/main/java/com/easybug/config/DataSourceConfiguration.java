e4f05c1cb7f04c0712e4afe61a2c3bc4e4442589
liujieVS <ljwelldone@163.com>
2018-11-30 17:43:50 +0800

    实现登陆注册功能后台

--- dao/src/main/java/com/easybug/config/DataSourceConfiguration.java
+++ dao/src/main/java/com/easybug/config/DataSourceConfiguration.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 import java.beans.PropertyVetoException;
 
